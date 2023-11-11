ae46ec3f39a1d35f0eea0dc06457e9d303b6a4e7
Murat İlhan <murattilhann08@gmail.com>
2023-11-11 12:39:53 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask_cors import CORS
+from flask import Flask, make_response, jsonify
+
+app = Flask(__name__)
+CORS(app)
+
+
+
+@app.route('/start', methods=['GET'])
+def start():
+    return "bu bir get method2"
+
+@app.route('/post', methods=['POST'])
+def start2():
+    return "bu bir post method"
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+    
