d0b55829f4e0bbd19d87f199451a97d03e5b77f4
Murat İlhan <murattilhann08@gmail.com>
2023-11-22 13:25:43 +0300

    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,21 @@
 import streamlit as st
-st.write("asdsad")
+from flask_cors import CORS
+from flask import Flask, make_response, jsonify
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/test', methods=['GET'])
+def start2():
+    return jsonify({"message":"bu bir get method"})
+
+@app.route('/test', methods=['GET'])
+def start2():
+    return jsonify({"message":"bu bir get method"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
